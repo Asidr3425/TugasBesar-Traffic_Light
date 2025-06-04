e49a15062896fe04fa0355488070bfb96be96a5a
nifnip22 <proxigaming742@gmail.com>
2025-06-04 21:43:37 +0800

    feat: Called Frame into Main.java

--- src/trafficlight/Main.java
+++ src/trafficlight/Main.java
@@ -1,1 +1,11 @@
 package trafficlight;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+      new TrafficLightFrame();
+    });
+  }
+}
