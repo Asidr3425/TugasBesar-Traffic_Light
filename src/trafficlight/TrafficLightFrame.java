7301b15237edb5baf856630b1ed57dafd7597dd6
nifnip22 <proxigaming742@gmail.com>
2025-06-04 21:34:51 +0800

    feat: Create trafic light JPanel frame

--- src/trafficlight/TrafficLightFrame.java
+++ src/trafficlight/TrafficLightFrame.java
@@ -1,1 +1,32 @@
 package trafficlight;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TrafficLightFrame extends JFrame {
+  private TrafficLightPanel panel;
+
+  public TrafficLightFrame() {
+    super("Traffic Light");
+
+    panel = new TrafficLightPanel();
+    add(panel, BorderLayout.CENTER);
+
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setResizable(false);
+    pack();
+    setLocationRelativeTo(null);
+    setVisible(true);
+
+    /**
+     * TODO:
+     * Add a timer here (javax.swing.Timer) to change the traffic light state every
+     * 5 seconds and call panel.setTrafficLightState(...). Order: RED -> YELLOW ->
+     * GREEN -> RED
+     */
+  }
+
+  public TrafficLightPanel getTrafficLightPanel() {
+    return panel;
+  }
+}
